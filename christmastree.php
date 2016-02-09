25d52c5fce28fc8eeb7f55b76c902a033d72f199
jmoritdev <jmmt03@gmail.com>
2016-02-09 14:16:48 +0100

    uploaded christmastree.php
    for drawing a christmastree and converting a while loop into a for loop.

---
+++ christmastree.php
@@ -0,0 +1,16 @@
+<?php
+
+for ($height = 0; $height < 10; $height++) {
+    for ($amountOfSpaces = 20; $amountOfSpaces > $height * 2; $amountOfSpaces -= 2) {
+        echo "&nbsp";
+    }
+    for ($amountOfAsterisks = 0; $amountOfAsterisks < $height; $amountOfAsterisks++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+
+for($x  = 35; $x >= 25; $x--) {
+    echo "hoppelepee";
+}
+
